f9501bb0447e4b592e8a5134f03715ca1d50735f
Jokemaker-afk <jackye.zheng@mail.utoronto.ca>
2024-10-19 16:11:15 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the Current User as name "name".
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
